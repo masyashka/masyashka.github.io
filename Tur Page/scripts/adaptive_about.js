ab1ce2d97427af5c58268a55efa9a4028aa9eda1
masyashka <mariyapetrenko2000@yandex.ru>
2018-10-27 15:19:49 +0300

    Remake html

--- Tur Page/scripts/adaptive_about.js
+++ Tur Page/scripts/adaptive_about.js
@@ -1,4 +1,4 @@
-remake_rectangle();
+// remake_rectangle();
 
 function remake_rectangle() {
 	document.getElementById('rec_1').style.height = (document.getElementById('im_1').offsetHeight) / 3 + "px";
@@ -6,5 +6,3 @@ function remake_rectangle() {
 	document.getElementById('rec_3').style.height = (document.getElementById('im_1').offsetHeight) / 3 + "px";
 	document.getElementById('rec_4').style.height = (document.getElementById('im_1').offsetHeight) / 3 + "px";
 }
-
-// <script src="scripts/adaptive_about.js"></script>
--- Tur Page/scripts/adaptive_height.js
+++ Tur Page/scripts/adaptive_height.js
@@ -1,4 +1,7 @@
-make_equal_heights();
+// make_equal_heights();
+$(document).ready(function() {
+	make_equal_heights();
+});
 
 function make_equal_heights() {
 	remake_rectangle();
